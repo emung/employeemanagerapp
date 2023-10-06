f9142001c9b47f732b5826eb173d6291bbefc7e3
Emanuel <emanuel.ungheanu@prolion.com>
2023-10-06 11:53:35 +0200

    EM-1 add Employee service & employee dto

---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Employee} from "./employee";
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private apiServerUrl = '';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getAllEmployees(): Observable<Employee[]> {
+    return this.httpClient.get<Employee[]>(`${this.apiServerUrl}/employees`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.post<Employee>(`${this.apiServerUrl}/employees`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.httpClient.put<Employee>(`${this.apiServerUrl}/employees`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiServerUrl}/employees/${employeeId}`);
+  }
+
+
+}
